388189e1ba6fe8f7e96454773a75ebae347ec054
Tiago Angelo <tiago.angelo@zup.com.br>
2021-03-19 15:46:21 -0300

    Optional Jaeger installation

--- jaeger-operator.tf
+++ jaeger-operator.tf
@@ -1,22 +1,28 @@
 resource "kubernetes_namespace" "tracing" {
+  count = var.jaeger_enabled ? 1 : 0
+
   metadata {
     name = "tracing"
   }
 }
 
 resource "helm_release" "jaeger_operator" {
+  count = var.jaeger_enabled ? 1 : 0
+
   name = "jaeger-operator"
   chart = "https://github.com/jaegertracing/helm-charts/releases/download/jaeger-operator-2.19.1/jaeger-operator-2.19.1.tgz"
-  namespace = kubernetes_namespace.tracing.metadata[0].name
+  namespace = kubernetes_namespace.tracing[0].metadata[0].name
 }
 
 resource "kustomization_resource" "jaeger_custom_resource" {
+  count = var.jaeger_enabled ? 1 : 0
+
   manifest = jsonencode({
     apiVersion = "jaegertracing.io/v1"
     kind = "Jaeger"
     metadata = {
       name = "jaeger"
-      namespace = kubernetes_namespace.tracing.metadata[0].name
+      namespace = kubernetes_namespace.tracing[0].metadata[0].name
     }
     spec = {
       ingress = { enabled = true, hosts = [ "tracing.zup" ] }

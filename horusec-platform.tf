93ec739c67ef7ff1ee2fe32e214a86f5d69b90d5
Tiago Angelo <tiago.angelo@zup.com.br>
2021-06-25 12:02:05 -0300

    Configure SMTP server

--- horusec-platform.tf
+++ horusec-platform.tf
@@ -15,6 +15,7 @@ data "template_file" "horusec_platform" {
     RABBITMQ_HOST = "rabbitmq.${kubernetes_namespace.queue.metadata[0].name}.svc.cluster.local"
 
     JWT_SECRET_NAME = kubernetes_secret.horusec_jwt.metadata[0].name
+    SMTP_SECRET_NAME = kubernetes_secret.horusec_smtp.metadata[0].name
     RABBITMQ_SECRET_NAME = kubernetes_secret.horusec_broker.metadata[0].name
     ANALYTIC_DB_SECRET_NAME = kubernetes_secret.analytic_db.metadata[0].name
     PLATFORM_DB_SECRET_NAME = kubernetes_secret.platform_db.metadata[0].name

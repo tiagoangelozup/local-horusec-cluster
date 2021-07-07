865326ec34cbcd77d8eb90ff461d94246c8a620e
Tiago Angelo <tiago.angelo@zup.com.br>
2021-07-07 10:12:06 -0300

    Wait for namespace

--- horusec-admin.tf
+++ horusec-admin.tf
@@ -1,5 +1,5 @@
 data "kustomization_overlay" "horusec_admin" {
-  namespace = kubernetes_namespace.horusec_system.metadata[0].name
+  namespace = var.horusec_namespace
 
   resources = [
     "https://github.com/ZupIT/horusec-admin/deployments/k8s/overlays/production?ref=${var.horusec_admin_version}"
@@ -14,4 +14,8 @@ data "kustomization_overlay" "horusec_admin" {
 resource "kustomization_resource" "horusec_admin" {
   for_each = data.kustomization_overlay.horusec_admin.ids
   manifest = data.kustomization_overlay.horusec_admin.manifests[each.value]
+
+  depends_on = [
+    kubernetes_namespace.horusec_system
+  ]
 }
